0c029170c7eb5aeeaae717a79bd3f21e46033056
Thierno Diallo <thierno.241991@gmail.com>
2026-03-14 14:35:35 +0000

    pouur les icones qui tourne l'icone prisma et dans la section comptences

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -25,8 +25,9 @@ const TechIcons = {
     <div className="text-white font-bold italic w-10 h-10 flex items-center justify-center text-2xl drop-shadow-xl">ex</div>
   ),
   Prisma: () => (
-    <svg viewBox="0 0 256 256" className="w-10 h-10 fill-white">
-      <path d="M128 0l128 128-128 128L0 128 128 0z"/>
+    <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" className="w-10 h-10">
+      <title>file_type_light_prisma</title>
+      <path d="M25.21,24.21,12.739,27.928a.525.525,0,0,1-.667-.606L16.528,5.811a.43.43,0,0,1,.809-.094l8.249,17.661A.6.6,0,0,1,25.21,24.21Zm2.139-.878L17.8,2.883h0A1.531,1.531,0,0,0,16.491,2a1.513,1.513,0,0,0-1.4.729L4.736,19.648a1.592,1.592,0,0,0,.018,1.7l5.064,7.909a1.628,1.628,0,0,0,1.83.678l14.7-4.383a1.6,1.6,0,0,0,1-2.218Z" fill="#0c344b" fillRule="evenodd" />
     </svg>
   ),
   SocketIO: () => (
