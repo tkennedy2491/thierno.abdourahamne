45ee0cb903cbcd0d03bdc27676b174dbc907f553
Thierno Diallo <thierno.241991@gmail.com>
2026-03-14 14:15:54 +0000

    dans la partie "Compétences et services" l'icone aws tu peux remplacer p

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -213,68 +213,68 @@ export function Skills() {
           <path d="M14.8442 10.7893C14.9752 10.6668 15.3878 10.3333 15.9315 10.6736C16.0494 10.7416 16.1673 10.8165 16.3376 10.9186C16.6848 11.1296 16.5145 11.4358 16.0952 11.6264C15.9053 11.7081 15.5909 11.885 15.3551 11.8714C15.0931 11.8442 14.9162 11.6672 14.7459 11.5515C14.4249 11.3338 14.4446 11.15 14.5952 11.0071C14.7066 10.8982 14.8311 10.7961 14.8442 10.7893Z" fill="url(#paint15_linear_87_7435_skills)"></path>
           <defs>
             <linearGradient id="paint0_linear_87_7435_skills" x1="16.0578" y1="6.30617" x2="15.8364" y2="9.30598" gradientUnits="userSpaceOnUse">
-              <stop stopColor="white" stopOpacity="0.8"></stop>
-              <stop offset="1" stopColor="white" stopOpacity="0"></stop>
+              <stop stopColor="white" stopOpacity={0.8}></stop>
+              <stop offset={1} stopColor="white" stopOpacity={0}></stop>
             </linearGradient>
             <linearGradient id="paint1_linear_87_7435_skills" x1="12.8966" y1="22.6721" x2="11.2506" y2="17.2716" gradientUnits="userSpaceOnUse">
               <stop stopColor="#FFEED7"></stop>
-              <stop offset="1" stopColor="#BDBFC2"></stop>
+              <stop offset={1} stopColor="#BDBFC2"></stop>
             </linearGradient>
             <linearGradient id="paint2_linear_87_7435_skills" x1="12.8966" y1="22.6721" x2="11.2506" y2="17.2716" gradientUnits="userSpaceOnUse">
               <stop stopColor="#FFEED7"></stop>
-              <stop offset="1" stopColor="#BDBFC2"></stop>
+              <stop offset={1} stopColor="#BDBFC2"></stop>
             </linearGradient>
             <linearGradient id="paint3_linear_87_7435_skills" x1="16.6681" y1="9.65546" x2="16.5748" y2="10.3542" gradientUnits="userSpaceOnUse">
-              <stop stopColor="white" stopOpacity="0.65"></stop>
-              <stop offset="1" stopColor="white" stopOpacity="0"></stop>
+              <stop stopColor="white" stopOpacity={0.65}></stop>
+              <stop offset={1} stopColor="white" stopOpacity={0}></stop>
             </linearGradient>
             <linearGradient id="paint4_linear_87_7435_skills" x1="14.434" y1="9.78294" x2="14.5344" y2="10.3483" gradientUnits="userSpaceOnUse">
-              <stop stopColor="white" stopOpacity="0.65"></stop>
-              <stop offset="1" stopColor="white" stopOpacity="0"></stop>
+              <stop stopColor="white" stopOpacity={0.65}></stop>
+              <stop offset={1} stopColor="white" stopOpacity={0}></stop>
             </linearGradient>
             <linearGradient id="paint5_linear_87_7435_skills" x1="15.7613" y1="15.6306" x2="15.778" y2="19.6272" gradientUnits="userSpaceOnUse">
-              <stop stopColor="white" stopOpacity="0.8"></stop>
-              <stop offset="1" stopColor="white" stopOpacity="0"></stop>
+              <stop stopColor="white" stopOpacity={0.8}></stop>
+              <stop offset={1} stopColor="white" stopOpacity={0}></stop>
             </linearGradient>
             <linearGradient id="paint6_linear_87_7435_skills" x1="15.8504" y1="13.1247" x2="15.8688" y2="14.7138" gradientUnits="userSpaceOnUse">
-              <stop stopColor="white" stopOpacity="0.65"></stop>
-              <stop offset="1" stopColor="white" stopOpacity="0"></stop>
+              <stop stopColor="white" stopOpacity={0.65}></stop>
+              <stop offset={1} stopColor="white" stopOpacity={0}></stop>
             </linearGradient>
             <linearGradient id="paint7_linear_87_7435_skills" x1="11.72" y1="14.1055" x2="11.72" y2="19.9372" gradientUnits="userSpaceOnUse">
-              <stop stopColor="white" stopOpacity="0.65"></stop>
-              <stop offset="1" stopColor="white" stopOpacity="0"></stop>
+              <stop stopColor="white" stopOpacity={0.65}></stop>
+              <stop offset={1} stopColor="white" stopOpacity={0}></stop>
             </linearGradient>
             <linearGradient id="paint8_linear_87_7435_skills" x1="21.033" y1="13.5359" x2="21.0308" y2="18.8052" gradientUnits="userSpaceOnUse">
-              <stop stopColor="white" stopOpacity="0.65"></stop>
-              <stop offset="1" stopColor="white" stopOpacity="0"></stop>
+              <stop stopColor="white" stopOpacity={0.65}></stop>
+              <stop offset={1} stopColor="white" stopOpacity={0}></stop>
             </linearGradient>
             <linearGradient id="paint9_linear_87_7435_skills" x1="11.4286" y1="22.4374" x2="10.5354" y2="25.4214" gradientUnits="userSpaceOnUse">
               <stop stopColor="#FFA63F"></stop>
-              <stop offset="1" stopColor="#FFFF00"></stop>
+              <stop offset={1} stopColor="#FFFF00"></stop>
             </linearGradient>
             <linearGradient id="paint10_linear_87_7435_skills" x1="19.882" y1="21.53" x2="22.2656" y2="24.7801" gradientUnits="userSpaceOnUse">
               <stop stopColor="#FFA63F"></stop>
-              <stop offset="1" stopColor="#FFFF00"></stop>
+              <stop offset={1} stopColor="#FFFF00"></stop>
             </linearGradient>
             <linearGradient id="paint11_linear_87_7435_skills" x1="20.5198" y1="18.9333" x2="19.6863" y2="22.8605" gradientUnits="userSpaceOnUse">
-              <stop stopColor="white" stopOpacity="0.65"></stop>
-              <stop offset="1" stopColor="white" stopOpacity="0"></stop>
+              <stop stopColor="white" stopOpacity={0.65}></stop>
+              <stop offset={1} stopColor="white" stopOpacity={0}></stop>
             </linearGradient>
             <linearGradient id="paint12_linear_87_7435_skills" x1="11.2464" y1="19.9042" x2="11.4117" y2="24.3239" gradientUnits="userSpaceOnUse">
-              <stop stopColor="white" stopOpacity="0.65"></stop>
-              <stop offset="1" stopColor="white" stopOpacity="0"></stop>
+              <stop stopColor="white" stopOpacity={0.65}></stop>
+              <stop offset={1} stopColor="white" stopOpacity={0}></stop>
             </linearGradient>
             <linearGradient id="paint13_linear_87_7435_skills" x1="20.3755" y1="18.8616" x2="20.5688" y2="20.1822" gradientUnits="userSpaceOnUse">
-              <stop stopColor="white" stopOpacity="0.65"></stop>
-              <stop offset="1" stopColor="white" stopOpacity="0"></stop>
+              <stop stopColor="white" stopOpacity={0.65}></stop>
+              <stop offset={1} stopColor="white" stopOpacity={0}></stop>
             </linearGradient>
             <linearGradient id="paint14_linear_87_7435_skills" x1="15.7644" y1="10.784" x2="15.7805" y2="13.1098" gradientUnits="userSpaceOnUse">
               <stop stopColor="#FFA63F"></stop>
-              <stop offset="1" stopColor="#FFFF00"></stop>
+              <stop offset={1} stopColor="#FFFF00"></stop>
             </linearGradient>
             <linearGradient id="paint15_linear_87_7435_skills" x1="15.5106" y1="10.5638" x2="15.5062" y2="11.7936" gradientUnits="userSpaceOnUse">
-              <stop stopColor="white" stopOpacity="0.65"></stop>
-              <stop offset="1" stopColor="white" stopOpacity="0"></stop>
+              <stop stopColor="white" stopOpacity={0.65}></stop>
+              <stop offset={1} stopColor="white" stopOpacity={0}></stop>
             </linearGradient>
           </defs>
         </svg>
@@ -303,9 +303,16 @@ export function Skills() {
       title: 'AWS',
       description: t.skills.s_aws,
       icon: (
-        <svg viewBox="0 0 256 154" className="w-16 h-16 fill-[#FF9900]">
-          <path d="M112.5 101.4c-9.1 0-16.3-2-21.7-6.1-5.4-4-8.1-10-8.1-17.8 0-7.8 2.7-13.7 8.1-17.8 5.4-4 12.6-6.1 21.7-6.1s16.3 2 21.7 6.1c5.4 4 8.1 10 8.1 17.8 0 7.8-2.7 13.8-8.1 17.8-5.4 4.1-12.6 6.1-21.7 6.1z"/>
-          <path d="M256 122.3l-10.8-15.3c-2.8-4-6.2-4.1-8.5-4.1h-44.5c-4.4 0-7.7 2.2-7.7 6.3 0 2.2.9 4.2 2.6 5.8l10.8 10.3c3.4 3.2 8.1 4.7 12.8 4.7h31.1c5.7 0 10.8-3.4 12.8-8l1.4-3.7v3.7h1.4v0.3z"/>
+        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="none" className="w-16 h-16">
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+          <g id="SVGRepo_iconCarrier">
+            <path fill="#252F3E" d="M4.51 7.687c0 .197.02.357.058.475.042.117.096.245.17.384a.233.233 0 01.037.123c0 .053-.032.107-.1.16l-.336.224a.255.255 0 01-.138.048c-.054 0-.107-.026-.16-.074a1.652 1.652 0 01-.192-.251 4.137 4.137 0 01-.165-.315c-.415.491-.936.737-1.564.737-.447 0-.804-.129-1.064-.385-.261-.256-.394-.598-.394-1.025 0-.454.16-.822.484-1.1.325-.278.756-.416 1.304-.416.18 0 .367.016.564.042.197.027.4.07.612.118v-.39c0-.406-.085-.689-.25-.854-.17-.166-.458-.246-.868-.246-.186 0-.377.022-.574.07a4.23 4.23 0 00-.575.181 1.525 1.525 0 01-.186.07.326.326 0 01-.085.016c-.075 0-.112-.054-.112-.166v-.262c0-.085.01-.15.037-.186a.399.399 0 01.15-.113c.185-.096.409-.176.67-.24.26-.07.537-.101.83-.101.633 0 1.096.144 1.394.432.293.288.442.726.442 1.314v1.73h.01zm-2.161.811c.175 0 .356-.032.548-.096.191-.064.362-.182.505-.342a.848.848 0 00.181-.341c.032-.129.054-.283.054-.465V7.03a4.43 4.43 0 00-.49-.09 3.996 3.996 0 00-.5-.033c-.357 0-.618.07-.793.214-.176.144-.26.347-.26.614 0 .25.063.437.196.566.128.133.314.197.559.197zm4.273.577c-.096 0-.16-.016-.202-.054-.043-.032-.08-.106-.112-.208l-1.25-4.127a.938.938 0 01-.049-.214c0-.085.043-.133.128-.133h.522c.1 0 .17.016.207.053.043.032.075.107.107.208l.894 3.535.83-3.535c.026-.106.058-.176.1-.208a.365.365 0 01.214-.053h.425c.102 0 .17.016.213.053.043.032.08.107.101.208l.841 3.578.92-3.578a.458.458 0 01.107-.208.346.346 0 01.208-.053h.495c.085 0 .133.043.133.133 0 .027-.006.054-.01.086a.76.76 0 01-.038.133l-1.283 4.127c-.032.107-.069.177-.111.209a.34.34 0 01-.203.053h-.457c-.101 0-.17-.016-.213-.053-.043-.038-.08-.107-.101-.214L8.213 5.37l-.82 3.439c-.026.107-.058.176-.1.213-.043.038-.118.054-.213.054h-.458zm6.838.144a3.51 3.51 0 01-.82-.096c-.266-.064-.473-.134-.612-.214-.085-.048-.143-.101-.165-.15a.378.378 0 01-.031-.149v-.272c0-.112.042-.166.122-.166a.3.3 0 01.096.016c.032.011.08.032.133.054.18.08.378.144.585.187.213.042.42.064.633.064.336 0 .596-.059.777-.176a.575.575 0 00.277-.508.52.52 0 00-.144-.373c-.095-.102-.276-.193-.537-.278l-.772-.24c-.388-.123-.676-.305-.851-.545a1.275 1.275 0 01-.266-.774c0-.224.048-.422.143-.593.096-.17.224-.32.384-.438.16-.122.34-.213.553-.277.213-.064.436-.091.67-.091.118 0 .24.005.357.021.122.016.234.038.346.06.106.026.208.052.303.085.096.032.17.064.224.096a.46.46 0 01.16.133.289.289 0 01.047.176v.251c0 .112-.042.171-.122.171a.552.552 0 01-.202-.064 2.427 2.427 0 00-1.022-.208c-.303 0-.543.048-.708.15-.165.1-.25.256-.25.475 0 .149.053.277.16.379.106.101.303.202.585.293l.756.24c.383.123.66.294.825.513.165.219.244.47.244.748 0 .23-.047.437-.138.619a1.436 1.436 0 01-.388.47c-.165.133-.362.23-.591.299-.24.075-.49.112-.761.112z"></path>
+            <g fill="#F90" fillRule="evenodd" clipRule="evenodd">
+              <path d="M14.465 11.813c-1.75 1.297-4.294 1.986-6.481 1.986-3.065 0-5.827-1.137-7.913-3.027-.165-.15-.016-.353.18-.235 2.257 1.313 5.04 2.109 7.92 2.109 1.941 0 4.075-.406 6.039-1.239.293-.133.543.192.255.406z"></path>
+              <path d="M15.194 10.98c-.223-.287-1.479-.138-2.048-.069-.17.022-.197-.128-.043-.24 1-.705 2.645-.502 2.836-.267.192.24-.053 1.89-.99 2.68-.143.123-.281.06-.218-.1.213-.53.687-1.72.463-2.003z"></path>
+            </g>
+          </g>
         </svg>
       ),
       borderColor: 'border-b-[#FF9900]'
